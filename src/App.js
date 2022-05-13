867fe785a8ab2483db8fe346d16f281336beda74
unknown <thdgus4025@gmail.com>
2022-05-14 00:02:12 +0900

    add html and useState

---
+++ src/App.js
@@ -0,0 +1,103 @@
+/* eslint-disable */
+
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  
+  let [title, setTitle] = useState(['안녕하세요' , '식사하셨습니까', '안녕히계세요']);
+  let [like, setLike] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(false); // 모달 초기값은 false로 지정
+  let modalToggle = () => {
+    setModal(modal => modal ? false : true);
+  }
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4 style={{fontSize: '20px'}}>React Blog</h4>
+      </div>
+
+      {/* <div className='list'>
+        <h4><p  onClick={()=>{
+          modalToggle();
+        }} style={{cursor: 'pointer', display: 'inline'}}>{title[0]}</ p> <span className="like" onClick={() => {
+          setLike(like + 1);
+        }}>👍</span> {like} </h4>
+        <p>4월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{title[1]}</h4>
+        <p>4월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{title[2]}</h4>
+        <p>4월 17일 발행</p>
+      </div> */}
+
+      {
+        // 반복문을 사용하고 싶지만 html사이에 for문을 사용할 수 없음.
+        title.map(function(parameter, i){
+          return (
+            <div className='list' key = {i}>
+              <h4><p  onClick={()=>{
+                modalToggle();
+              }} style={{cursor: 'pointer', display: 'inline'}}>{title[i]}</ p> 
+              <span className="like" onClick={() => {
+                let likeCopy = [...like];
+                likeCopy[i] = likeCopy[i] + 1;
+                setLike(likeCopy);
+              }}>👍</span> {like[i]} </h4>
+              <p>4월 17일 발행</p>
+            </div>
+          );
+        })
+      }
+      
+      {
+        // if문은 html중간에 사용할 수 없음. 삼항연산자로 해야함
+        // 사용법: 조건식? 참일때 실행 : 거짓일때 실행
+        modal == true ? <Modal color={'lightGrey'} 작명={title} /> : null
+        // 작명은 보통 오른쪽이름 그대로 갖다붙임 ex) title={title}
+      }
+
+      {/* 
+      Component 언제사용?
+      1. 반복되는 html태그를 축약할 때.
+      2. 큰 페이지들
+      3. 자주변경되는 html-ui 들 (성능상 좋을 수 있음)
+      */}
+      
+      <button onClick={()=>{
+        // 제목을 가나다순으로 정렬하시오
+        let copy = [...title];
+        copy = copy.sort();
+        setTitle(copy);
+      }}>가나다순정렬</button>
+
+      <button onClick={() => {
+        let copy = [...title];
+        copy[0] = '변경된제목이다!';
+        setTitle(copy);
+      }}>타이틀변경</button>
+
+    </div>
+  );
+}
+
+function Modal(props){
+  return(
+    <>
+      <div className='modal' style={{background : props.color}}>
+        <h4>{props.작명[0]}</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+        <button onClick={()=>{
+          
+        }}>글수정</button>
+      </div> 
+    </>
+  )
+}
+
+export default App;
