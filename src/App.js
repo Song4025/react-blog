487858ea64b0f2da05be46070fac3256643b49a8
Song4025 <thdgus4025@gmail.com>
2022-05-14 22:03:39 +0900

    add input and addBtn

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import { useState } from 'react';
 function App() {
   
   let [title, setTitle] = useState(['안녕하세요' , '식사하셨습니까', '안녕히계세요']);
+  let [changeTitle, setChageTitle] = useState(0);
+
   let [like, setLike] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false); // 모달 초기값은 false로 지정
+
+  let [addText, setAddText] = useState('');
   let modalToggle = () => {
     setModal(modal => modal ? false : true);
   }
@@ -39,9 +43,11 @@ function App() {
         // 반복문을 사용하고 싶지만 html사이에 for문을 사용할 수 없음.
         title.map(function(parameter, i){
           return (
-            <div className='list' key = {i}>
-              <h4><p  onClick={()=>{
-                modalToggle();
+            <div className='list' key={i}>
+              <h4><p  onClick={(e)=>{
+                modalToggle()
+                setChageTitle(i)
+                e.stopPropagation()
               }} style={{cursor: 'pointer', display: 'inline'}}>{title[i]}</ p> 
               <span className="like" onClick={() => {
                 let likeCopy = [...like];
@@ -49,6 +55,9 @@ function App() {
                 setLike(likeCopy);
               }}>👍</span> {like[i]} </h4>
               <p>4월 17일 발행</p>
+              <button onClick={()=>{
+                
+              }}>삭제</button>
             </div>
           );
         })
@@ -57,7 +66,7 @@ function App() {
       {
         // if문은 html중간에 사용할 수 없음. 삼항연산자로 해야함
         // 사용법: 조건식? 참일때 실행 : 거짓일때 실행
-        modal == true ? <Modal color={'lightGrey'} 작명={title} setTitle={setTitle} /> : null
+        modal == true ? <Modal color={'lightGrey'} 작명={title} setTitle={setTitle} changeTitle={changeTitle} /> : null
         // 작명은 보통 오른쪽이름 그대로 갖다붙임 ex) title={title}
       }
 
@@ -67,9 +76,19 @@ function App() {
       2. 큰 페이지들
       3. 자주변경되는 html-ui 들 (성능상 좋을 수 있음)
       */}
+      <div>
+        <input type="text"  onChange={(e)=>{
+          setAddText(e.target.value)
+          console.log(addText)
+        }}></input>
+        <button onClick={()=>{
+          let copy = [...title];
+          copy.unshift(addText);
+          setTitle(copy)
+        }}>추가</button>
+      </div>
       
       <button onClick={()=>{
-        // 제목을 가나다순으로 정렬하시오
         let copy = [...title];
         copy = copy.sort();
         setTitle(copy);
@@ -86,20 +105,18 @@ function App() {
 }
 
 function Modal(props){
-  return(
-    <>
-      <div className='modal' style={{background : props.color}}>
-        <h4>{props.작명[0]}</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
-        <button onClick={()=>{
-          // props로 받아온 텍스트가 변경되게 해주세요.
-          let copy = [...props.작명];
-          copy[0] = '변경된 제목!!';
-          props.setTitle(copy);
-        }}>글수정</button>
-      </div> 
-    </>
+  return (
+    <div className='modal' style={{background : props.color}}>
+      <h4>{props.작명[props.changeTitle]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button onClick={()=>{
+        // props로 받아온 텍스트가 변경되게 해주세요.
+        let copy = [...props.작명];
+        copy[0] = '변경된 제목!!';
+        props.setTitle(copy);
+      }}>글수정</button>
+    </div>
   )
 }
 
